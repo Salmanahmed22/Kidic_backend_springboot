7e399b607f3e026c6fc01ffa61aac2ba609b7fed
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-09-04 17:43:11 +0300

    enviroment setup

---
+++ src/main/java/org/codewithkidic/kidic_backend/KidicBackendApplication.java
@@ -0,0 +1,13 @@
+package org.codewithkidic.kidic_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/codewithkidic/kidic_backend/KidicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codewithkidic.kidic_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
