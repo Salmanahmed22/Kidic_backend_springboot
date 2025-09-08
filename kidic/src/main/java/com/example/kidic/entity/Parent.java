2fdafab21ff18d8eca6fd1a242a9b748c6f670f9
Salmanahmed22 <123300970+Salmanahmed22@users.noreply.github.com>
2025-09-08 21:36:08 +0300

    add some dependencies and make parent extends userdetails

--- kidic/src/main/java/com/example/kidic/entity/Parent.java
+++ kidic/src/main/java/com/example/kidic/entity/Parent.java
@@ -4,13 +4,17 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "parents")
-public class Parent {
-    
+public class Parent implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -130,5 +134,36 @@ public class Parent {
     
     public enum ProfilePictureType {
         DEFAULT, CUSTOM, AVATAR_1, AVATAR_2, AVATAR_3
+    }
+
+    //Auth methods
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
